d751579041ea061d9e3c275133ad020339462d1b
Tim Park <timfpark@gmail.com>
2018-03-07 08:19:05 -0800

    add oldschool method for running jobs

---
+++ src/main/scala/backupLocations.scala
@@ -0,0 +1,49 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// scalastyle:off println
+package io.rhom
+
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.cassandra._
+
+import com.datastax.spark.connector.cql.CassandraConnectorConf
+import com.datastax.spark.connector.rdd.ReadConf
+
+/** Computes an approximation to pi */
+object BackupLocations {
+  def main(args: Array[String]) {
+    val spark = SparkSession
+      .builder
+      .appName("BackupLocations")
+      .getOrCreate()
+
+    val df = spark
+      .read
+      .format("org.apache.spark.sql.cassandra")
+      .options(Map( "table" -> "locations", "keyspace" -> "rhom"))
+      .load()
+
+    df.write
+      .mode("overwrite")
+      .format("com.databricks.spark.avro")
+      .save("wasb://backups@rhomlocations.blob.core.windows.net/locations/test")
+
+    spark.stop()
+  }
+}
+// scalastyle:on println
